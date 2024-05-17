49eed9748769480698e68e43392915fa3b0283e1
wxingfu <wxffxw.664587@gmail.com>
2024-05-17 19:03:19 +0800

    加控件和布局

--- Scanner/FormScan.Designer.cs
+++ Scanner/FormScan.Designer.cs
@@ -30,45 +30,133 @@
         {
             this.cbbDataSource = new System.Windows.Forms.ComboBox();
             this.bntScan = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.splitContainer = new System.Windows.Forms.SplitContainer();
+            this.panelSacnMenu = new System.Windows.Forms.Panel();
+            this.tabControlPreview = new System.Windows.Forms.TabControl();
+            this.tabPageSmallPreview = new System.Windows.Forms.TabPage();
+            this.tabPageVerticalPreview = new System.Windows.Forms.TabPage();
+            this.tabPageHorizontalPreView = new System.Windows.Forms.TabPage();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
+            this.splitContainer.Panel1.SuspendLayout();
+            this.splitContainer.Panel2.SuspendLayout();
+            this.splitContainer.SuspendLayout();
+            this.panelSacnMenu.SuspendLayout();
+            this.tabControlPreview.SuspendLayout();
             this.SuspendLayout();
             // 
             // cbbDataSource
             // 
+            this.cbbDataSource.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.cbbDataSource.FormattingEnabled = true;
-            this.cbbDataSource.Location = new System.Drawing.Point(13, 12);
+            this.cbbDataSource.Location = new System.Drawing.Point(12, 12);
+            this.cbbDataSource.Margin = new System.Windows.Forms.Padding(4);
             this.cbbDataSource.Name = "cbbDataSource";
-            this.cbbDataSource.Size = new System.Drawing.Size(254, 20);
+            this.cbbDataSource.Size = new System.Drawing.Size(226, 23);
             this.cbbDataSource.TabIndex = 1;
             // 
             // bntScan
             // 
-            this.bntScan.Location = new System.Drawing.Point(303, 12);
+            this.bntScan.Location = new System.Drawing.Point(12, 58);
+            this.bntScan.Margin = new System.Windows.Forms.Padding(4);
             this.bntScan.Name = "bntScan";
-            this.bntScan.Size = new System.Drawing.Size(60, 20);
+            this.bntScan.Size = new System.Drawing.Size(80, 25);
             this.bntScan.TabIndex = 2;
             this.bntScan.Text = "扫描";
             this.bntScan.UseVisualStyleBackColor = true;
             this.bntScan.Click += new System.EventHandler(this.bntScan_Click);
             // 
-            // panel1
+            // splitContainer
             // 
-            this.panel1.AutoScroll = true;
-            this.panel1.Location = new System.Drawing.Point(13, 78);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(779, 437);
-            this.panel1.TabIndex = 3;
+            this.splitContainer.BackColor = System.Drawing.SystemColors.Control;
+            this.splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer.Name = "splitContainer";
+            // 
+            // splitContainer.Panel1
+            // 
+            this.splitContainer.Panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.splitContainer.Panel1.Controls.Add(this.panelSacnMenu);
+            // 
+            // splitContainer.Panel2
+            // 
+            this.splitContainer.Panel2.Controls.Add(this.tabControlPreview);
+            this.splitContainer.Size = new System.Drawing.Size(1411, 698);
+            this.splitContainer.SplitterDistance = 286;
+            this.splitContainer.TabIndex = 3;
+            // 
+            // panelSacnMenu
+            // 
+            this.panelSacnMenu.BackColor = System.Drawing.SystemColors.Control;
+            this.panelSacnMenu.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panelSacnMenu.Controls.Add(this.bntScan);
+            this.panelSacnMenu.Controls.Add(this.cbbDataSource);
+            this.panelSacnMenu.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelSacnMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelSacnMenu.Name = "panelSacnMenu";
+            this.panelSacnMenu.Size = new System.Drawing.Size(286, 698);
+            this.panelSacnMenu.TabIndex = 0;
+            // 
+            // tabControlPreview
+            // 
+            this.tabControlPreview.Controls.Add(this.tabPageSmallPreview);
+            this.tabControlPreview.Controls.Add(this.tabPageVerticalPreview);
+            this.tabControlPreview.Controls.Add(this.tabPageHorizontalPreView);
+            this.tabControlPreview.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControlPreview.Location = new System.Drawing.Point(0, 0);
+            this.tabControlPreview.Name = "tabControlPreview";
+            this.tabControlPreview.SelectedIndex = 0;
+            this.tabControlPreview.Size = new System.Drawing.Size(1121, 698);
+            this.tabControlPreview.TabIndex = 0;
+            // 
+            // tabPageSmallPreview
+            // 
+            this.tabPageSmallPreview.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.tabPageSmallPreview.Location = new System.Drawing.Point(4, 25);
+            this.tabPageSmallPreview.Name = "tabPageSmallPreview";
+            this.tabPageSmallPreview.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageSmallPreview.Size = new System.Drawing.Size(1113, 669);
+            this.tabPageSmallPreview.TabIndex = 0;
+            this.tabPageSmallPreview.Text = "微缩视图";
+            this.tabPageSmallPreview.UseVisualStyleBackColor = true;
+            // 
+            // tabPageVerticalPreview
+            // 
+            this.tabPageVerticalPreview.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.tabPageVerticalPreview.Location = new System.Drawing.Point(4, 25);
+            this.tabPageVerticalPreview.Name = "tabPageVerticalPreview";
+            this.tabPageVerticalPreview.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageVerticalPreview.Size = new System.Drawing.Size(1113, 669);
+            this.tabPageVerticalPreview.TabIndex = 1;
+            this.tabPageVerticalPreview.Text = "垂直视图";
+            this.tabPageVerticalPreview.UseVisualStyleBackColor = true;
+            // 
+            // tabPageHorizontalPreView
+            // 
+            this.tabPageHorizontalPreView.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.tabPageHorizontalPreView.Location = new System.Drawing.Point(4, 25);
+            this.tabPageHorizontalPreView.Name = "tabPageHorizontalPreView";
+            this.tabPageHorizontalPreView.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageHorizontalPreView.Size = new System.Drawing.Size(1113, 669);
+            this.tabPageHorizontalPreView.TabIndex = 2;
+            this.tabPageHorizontalPreView.Text = "水平视图";
+            this.tabPageHorizontalPreView.UseVisualStyleBackColor = true;
             // 
             // FormScan
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(934, 577);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.bntScan);
-            this.Controls.Add(this.cbbDataSource);
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(1411, 698);
+            this.Controls.Add(this.splitContainer);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FormScan";
             this.Text = "FormScan";
+            this.splitContainer.Panel1.ResumeLayout(false);
+            this.splitContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
+            this.splitContainer.ResumeLayout(false);
+            this.panelSacnMenu.ResumeLayout(false);
+            this.tabControlPreview.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -76,7 +164,12 @@
         #endregion
         private System.Windows.Forms.ComboBox cbbDataSource;
         private System.Windows.Forms.Button bntScan;
-        private System.Windows.Forms.FlowLayoutPanel panel1;
+        private System.Windows.Forms.SplitContainer splitContainer;
+        private System.Windows.Forms.Panel panelSacnMenu;
+        private System.Windows.Forms.TabControl tabControlPreview;
+        private System.Windows.Forms.TabPage tabPageSmallPreview;
+        private System.Windows.Forms.TabPage tabPageVerticalPreview;
+        private System.Windows.Forms.TabPage tabPageHorizontalPreView;
     }
 }
 
--- Scanner/FormScan.cs
+++ Scanner/FormScan.cs
@@ -20,7 +20,7 @@ namespace Scanner
 
         private static string imageSuffix = FilePath.GetImageFormatExtension(ImageFormat.Tiff);
 
-        List<PictureBox> pictureBoxes = new List<PictureBox>();
+        private List<string> imagePathList = new List<string>();
 
         private static AreaSettings AreaSettings = new AreaSettings(Units.Centimeters, 0.1f, 5.7f, 0.1F + 2.6f, 5.7f + 2.6f);
 
@@ -32,10 +32,21 @@ namespace Scanner
         {
             InitializeComponent();
 
+            #region   初始化控件缩放
+            x = Width;
+            y = Height;
+            SetTag(this);
+            this.Resize += FormScan_Resize;
+            #endregion
+
             Init();
         }
 
+        #region 扫描处理
 
+        /// <summary>
+        /// 
+        /// </summary>
         private void Init()
         {
             try
@@ -53,49 +64,33 @@ namespace Scanner
                 Bitmap image = args.Image;
                 if (image != null)
                 {
-                    //string imageName = Guid.NewGuid().ToString() + imageSuffix;
-                    //string imagePath = fileBasePath + imageName;
-                    //log.Info("basePath: " + fileBasePath + ", imageName: " + imageName + ", imagePath: " + imagePath);
+                    string imageName = Guid.NewGuid().ToString() + imageSuffix;
+                    string imagePath = fileBasePath + imageName;
+                    log.Info("basePath: " + fileBasePath + ", imageName: " + imageName + ", imagePath: " + imagePath);
 
-                    //image.Save(imagePath);
-                    PictureBox picture = new PictureBox();
-                    picture.Image = image;
-                    pictureBoxes.Add(picture);
+                    image.Save(imagePath);
+
+                    // 记录图像地址
+                    imagePathList.Add(imagePath);
                 }
             };
 
             _twain.ScanningComplete += delegate
             {
                 Enabled = true;
-
-                pictureBoxes.ForEach(pictureBox =>
-                {
-                    Image thumbnailImage = pictureBox.Image.GetThumbnailImage(200, 200, new Image.GetThumbnailImageAbort(GetThumbnailImageCallBack), new IntPtr());
-                    PictureBox thumbnailBox = new PictureBox();
-                    thumbnailBox.Image = thumbnailImage;
-                    thumbnailBox.SizeMode = PictureBoxSizeMode.AutoSize;
-                    panel1.Controls.Add(thumbnailBox);
-                });
             };
 
 
             List<string> sourceNames = _twain.GetSourceNames();
             if (sourceNames != null && sourceNames.Count > 0)
             {
+                //下拉框绑定数据源并默认选中第一个
                 cbbDataSource.DataSource = sourceNames;
                 cbbDataSource.SelectedIndex = 0;
-
-                log.Info("DataSource: " + cbbDataSource.SelectedItem.ToString());
-
-                _twain.SelectSource(cbbDataSource.SelectedItem.ToString());
             }
 
         }
 
-        private bool GetThumbnailImageCallBack()
-        {
-            return true;
-        }
 
         /// <summary>
         /// 
@@ -106,8 +101,17 @@ namespace Scanner
         {
             Enabled = false;
 
-            pictureBoxes.Clear();
-            panel1.Controls.Clear();
+            //获取选中的数据源
+            object item = cbbDataSource.SelectedItem;
+            string sourceName = cbbDataSource.GetItemText(item);
+            if (string.IsNullOrEmpty(sourceName))
+            {
+                MessageBox.Show("没有可用的扫描仪");
+                return;
+            }
+            log.Info("选中的数据源是: " + sourceName);
+
+            _twain.SelectSource(sourceName);
 
             _settings = new ScanSettings();
             _settings.UseDocumentFeeder = true;
@@ -135,6 +139,96 @@ namespace Scanner
                 Enabled = true;
             }
         }
+        # endregion
+
+
+        #region 控件大小随窗体大小等比例缩放
+
+        //定义当前窗体的宽度
+        private readonly float x;
+
+        //定义当前窗体的高度
+        private readonly float y;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="cons"></param>
+        private void SetTag(Control cons)
+        {
+            foreach (Control con in cons.Controls)
+            {
+                con.Tag = con.Width + ";" + con.Height + ";" + con.Left + ";" + con.Top + ";" + con.Font.Size;
+                if (con.Controls.Count > 0)
+                {
+                    SetTag(con);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="newx"></param>
+        /// <param name="newy"></param>
+        /// <param name="cons"></param>
+        private void SetControls(float newx, float newy, Control cons)
+        {
+            //遍历窗体中的控件，重新设置控件的值
+            foreach (Control con in cons.Controls)
+                //获取控件的Tag属性值，并分割后存储字符串数组
+                if (con.Tag != null)
+                {
+                    var mytag = con.Tag.ToString().Split(';');
+                    //根据窗体缩放的比例确定控件的值
+                    //宽度
+                    con.Width = Convert.ToInt32(Convert.ToSingle(mytag[0]) * newx);
+                    //高度
+                    con.Height = Convert.ToInt32(Convert.ToSingle(mytag[1]) * newy);
+                    //左边距
+                    con.Left = Convert.ToInt32(Convert.ToSingle(mytag[2]) * newx);
+                    //顶边距
+                    con.Top = Convert.ToInt32(Convert.ToSingle(mytag[3]) * newy);
+                    //字体大小
+                    var currentSize = Convert.ToSingle(mytag[4]) * newy;
+
+                    if (currentSize > 0)
+                    {
+                        con.Font = new Font(con.Font.Name, currentSize, con.Font.Style, con.Font.Unit);
+                    }
+
+                    con.Focus();
+
+                    if (con.Controls.Count > 0)
+                    {
+                        SetControls(newx, newy, con);
+                    }
+                }
+        }
+
+
+        /// <summary>
+        /// 重置窗体布局
+        /// </summary>
+        private void ReWinformLayout()
+        {
+            var newx = Width / x;
+            var newy = Height / y;
+            SetControls(newx, newy, this);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FormScan_Resize(object sender, EventArgs e)
+        {
+            //重置窗口布局
+            ReWinformLayout();
+        }
+
+        #endregion
 
 
     }
